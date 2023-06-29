7abcbbefadec8e7782f5421d9d1d46cd38fd59ed
skval55 <skvaladez@gmail.com>
2023-06-29 14:32:48 -0600

    finished exercise

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Card.css";
 
-const Card = () => {};
+const Card = ({ code, img }) => {
+  const [{ angle, xPos, yPos }] = useState({
+    angle: Math.random() * 90 - 45,
+    xPos: Math.random() * 40 - 20,
+    yPos: Math.random() * 40 - 20,
+  });
+
+  const transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+
+  return <img src={img} alt={code} className="cardImg" style={{ transform }} />;
+};
 
 export default Card;
--- src/DrawCardDeck.js
+++ src/DrawCardDeck.js
@@ -1,50 +1,92 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Card from "./Card";
 import axios from "axios";
 import { v4 as uuid } from "uuid";
+import "./DrawCardDeck.css";
 
 const DrawCardDeck = () => {
   const [deck, setDeck] = useState({});
   const [cards, setCards] = useState([]);
-  const cardUrl =
-    "https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=1";
+  const [drawing, setDrawing] = useState(false);
+  const timerId = useRef();
+
+  if (drawing === true && deck.remaining === 0) {
+    setDrawing(false);
+    alert("Error: no cards remaining!");
+  }
+  // get deck of cards
   useEffect(function fetchDeckOnce() {
     async function fetchDeck() {
       const deckResults = await axios.get(
         "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
       );
-      setDeck(deckResults);
+      setDeck(deckResults.data);
     }
     fetchDeck();
   }, []);
-  useEffect(function fetchCard() {
-    async function drawCard() {
-      const cardResults = await axios.get(
-        `https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`
-      );
-      addCard(cardResults.cards[0]);
+
+  // set timer for drawing card
+  useEffect(
+    function setCounter() {
+      if (drawing === true) {
+        timerId.current = setInterval(async () => {
+          await drawCard();
+        }, 1000);
+      }
+
+      return function cleanUpClearTimer() {
+        clearInterval(timerId.current);
+      };
+    },
+    [drawing]
+  );
+
+  // draw card
+  async function drawCard() {
+    if (deck.remaining === 0) {
+      alert("Error: no cards remaining!");
+      return;
     }
-  });
+    const cardResults = await axios.get(
+      `https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`
+    );
+    setDeck({ ...deck, remaining: cardResults.data.remaining });
+    addCard(cardResults.data.cards[0]);
+  }
   console.log(deck);
 
+  // put cards on page
   const renderCards = () => {
     return (
-      <div>
-        {cards.map((card) => {
-          <Card key={card.id} deckId={deck.deck_id} />;
-        })}
+      <div className="CardHolder">
+        {cards.map((card) => (
+          <Card key={card.id} code={card.code} img={card.image} />
+        ))}
       </div>
     );
   };
 
+  // update set cards to add new card
   const addCard = (card) => {
     const newCard = { ...card, id: uuid() };
-    setCards = [...cards, newCard];
+    setCards([...cards, newCard]);
+    console.log(cards);
+  };
+
+  const keepDrawing = () => {
+    if (drawing === false) {
+      setDrawing(true);
+    } else {
+      setDrawing(false);
+    }
   };
 
   return (
     <>
-      <button>draw card</button>
+      <button onClick={drawCard}>draw card</button>
+      <button onClick={keepDrawing}>
+        {drawing === false ? "keep drawing" : "stop"}
+      </button>
       {renderCards()}
     </>
   );
